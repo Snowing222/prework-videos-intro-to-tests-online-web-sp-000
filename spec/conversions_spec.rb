60eab669f386f9b53d20d408aea243da75268cfa
elsa chen <snowing222@gmail.com>
2020-05-08 16:24:50 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -7,9 +7,9 @@ describe "conversions" do
       expect(grams).to eq(0.0)
     end
     
-    it 'given 1, returns 0.0' do
-      grams = ounces_to_grams(0)
-      expect(grams).to eq(0.0)
+    it 'given 1, returns 28.3495' do
+      grams = ounces_to_grams(1)
+      expect(grams).to eq(28.3495)
     end
   end
 end
